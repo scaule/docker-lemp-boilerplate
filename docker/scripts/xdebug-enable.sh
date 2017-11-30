3a8ad89ced65d43e0f6cb5603cb3f828431dcd5f
scaule <scaule@clever-age.com>
2017-11-30 15:58:13 +0100

    update xdebug enable

--- docker/scripts/xdebug-enable.sh
+++ docker/scripts/xdebug-enable.sh
@@ -1,17 +1,25 @@
 #/bin/bash
 
-# Create an alias on mac ip
-sudo ifconfig lo0 alias 10.254.254.254
-HOST_IP=10.254.254.254
+#Mac
+if [ "$(uname)" == "Darwin" ]; then
+  #Create an alias on mac
+  sudo ifconfig lo0 alias 10.254.254.254
+  HOST_IP=10.254.254.254
+#Linux
+else
+  HOST_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.Gateway}}{{end}}' infotbm_nginx)
+fi
+
+
 
 # Enable the extension
-docker exec symfony_app cp /etc/php/7.0/fpm/conf.d/xdebug.tpl.conf /etc/php/7.0/fpm/conf.d/20-xdebug.ini
-docker exec symfony_app sed -i "s/%HOST_IP%/$HOST_IP/g" /etc/php/7.0/fpm/conf.d/20-xdebug.ini
-docker exec symfony_app cp /etc/php/7.0/cli/conf.d/xdebug.tpl.conf /etc/php/7.0/cli/conf.d/20-xdebug.ini
-docker exec symfony_app sed -i "s/%HOST_IP%/$HOST_IP/g" /etc/php/7.0/cli/conf.d/20-xdebug.ini
+docker exec infotbm_nginx cp /etc/php/7.1/fpm/conf.d/xdebug.tpl.conf /etc/php/7.1/fpm/conf.d/20-xdebug.ini
+docker exec infotbm_nginx sed -i "s/%HOST_IP%/$HOST_IP/g" /etc/php/7.1/fpm/conf.d/20-xdebug.ini
+docker exec infotbm_nginx cp /etc/php/7.1/cli/conf.d/xdebug.tpl.conf /etc/php/7.1/cli/conf.d/20-xdebug.ini
+docker exec infotbm_nginx sed -i "s/%HOST_IP%/$HOST_IP/g" /etc/php/7.1/cli/conf.d/20-xdebug.ini
 
 # Restart fpm
-docker exec symfony_app sudo /etc/init.d/php7.0-fpm restart
+docker exec infotbm_nginx sudo /etc/init.d/php7.1-fpm restart
 
 echo ""
 echo "XDebug enabled !"
@@ -21,12 +29,11 @@ echo " menu -> run -> edit configuration"
 echo "   button '+' -> php remote debug"
 echo "     name: whatever, server: click on '...'"
 echo "       servers: new"
-echo "         name: robin (important !)"
-echo "         host: 127.0.0.1, port: 9000"
+echo "         name: infotbm (important !)"
+echo "         host: admin.local.infotbm.fr, port: 80"
 echo "         then apply the path mapping:"
-echo "           .../user -> /var/www/user"
+echo "           .../admin -> /var/www/admin"
 echo "           .../cms -> /var/www/cms"
 echo "           etc..."
 echo "Now in PHPStorm, click Run/Start listening"
 echo ""
-
